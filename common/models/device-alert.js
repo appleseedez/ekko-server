cd6cf24eb2274180e1b29b22b175748d966125b5
Ekko <appleseedez@gmail.com>
2016-07-02 17:26:40 +0800

    20160702

--- common/models/device-alert.js
+++ common/models/device-alert.js
@@ -3,13 +3,18 @@ module.exports = function(Devicealert) {
   //   ctx.instance && (ctx.instance.id = ctx.instance.device_id)
   //   next()
   // })
+  function validateEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+  }
+
   Devicealert.observe('after save',function(ctx,next){
     console.log('has been saved',ctx.instance.device_id);
     // var from = Devicealert.app.models.Email.dataSource.emailserver.transports[0].auth.user;
     // console.log('from',from);
 
     Devicealert.app.models.DeviceInfo.findOne({where:{device_id:ctx.instance.device_id}},function(err,result){
-      if (result && result.setup_operator) {
+      if (result && validateEmail(result.setup_operator)) {
         console.log('to:',result.setup_operator);
         Devicealert.app.models.Email.send({
           from:'vleboy@163.com',
